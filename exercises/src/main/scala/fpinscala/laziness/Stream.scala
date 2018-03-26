b7e0b8562699cc8e249da11c735a989dd65caf85
ktf <not.discostoo@gmail.com>
2018-03-25 20:21:38 -0700

    exercise 5.6

--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -44,7 +44,9 @@ trait Stream[+A] {
   def forAll(p: A => Boolean): Boolean =
     foldRight(true)((a, b) => p(a) && b)
 
-  def headOption: Option[A] = ???
+  // re-implement headOption using foldRight:
+  def headOption: Option[A] =
+    foldRight(None: Option[A])((h, _) => Some(h))
 
   // 5.7 map, filter, append, flatmap using foldRight. Part of the exercise is
   // writing your own function signatures.
